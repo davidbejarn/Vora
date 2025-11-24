55cca7e8ba7acf6dd1ba4da2f5abc4f7d78e8782
David Bejarano Díaz <jdbejaranod@itc.edu.co>
2025-11-23 22:08:04 -0500

    Delete Vora Ex/Vora Ex directory

--- Vora Ex/Vora Ex/vora/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Instalando dependencias para el chatbot...
-python -m pip install --upgrade pip
-pip install -r requerimientos.txt
-echo.
-echo ========================================
-echo  ¡Todo listo! Ya puedes ejecutar app.py
-echo ========================================
-pause
